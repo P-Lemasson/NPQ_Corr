8a259d868ab068937697daa70d7679fc761d32e9
P-Lemasson <lemasson.pierrick@gmail.com>
2024-02-07 18:04:21 +0100

    Update NPQ_Corr_Function.py

--- NPQ_Corr_Function.py
+++ NPQ_Corr_Function.py
@@ -12,22 +12,24 @@ import pandas as pd
 
 def NPQ_Corr (depth, PAR, MLD, Chla , bbp):
     """
-    Function to correct the effect of NPQ on the fluorescence signal 
+    Function to Correct the Effect of NPQ on the Fluorescence Signal 
     
-    The method is based on the recommendations of Xing et al. 2018: adding a light threshold (PAR = 15) to existing methods such as Sackmann 2008.
-    With the modification of Terrats 2020 consisting in applying the sigmoid proposed by Xing et al. 2018 for shallow mixing cases only under MLD.
+    This method is based on the recommendations of Xing et al. (2018), which involve adding a light threshold (PAR = 15) 
+    to existing methods such as Sackmann (2008). 
+    Additionally, it incorporates the modification proposed by Terrats (2020), which applies the sigmoid function suggested by Xing et al. (2018) 
+    for shallow mixing cases only under the mixed layer depth (MLD).
     
-    In deep mixing cases or in case of error with the sigmoid, the original method of Sackmann is applied, but above the light threshold.
     
-    Chla must be smooth by rolling median window 11 (Xing 2018)
-    PAR and bbp too
+    In cases of deep mixing or when encountering errors with the sigmoid function, the original method of Sackmann is applied.
+    
+    Chla, PAR, and bbp must undergo smoothing using a rolling median window of 11 (Xing, 2018).
     Chla,bbp,PAR ==> same vertical resolution
-    
-    (Here sigmoide and Fratio are not smoothed)
+.
+    (Note sigmoide and Fratio are not smoothed)
     -----
     return 
 
-    Fluorescence corrected for quenching
+    Fluorescence corrected for quenching (Fqc)
     Initial quenched fluorescence signal
     depth array
     ----
@@ -40,7 +42,6 @@ def NPQ_Corr (depth, PAR, MLD, Chla , bbp):
     
     # Compute F-ratio from Sackmann 2008 and Sigmoide from Xing et al. 2018  (Only above zPAR15)
     Fratio = Chla[:np.absolute(PAR-15).argmin()+1]/bbp[:np.absolute(PAR-15).argmin()+1]
-    
     
     XB18 = Chla[:np.absolute(PAR-15).argmin()+1] / (0.092 + 0.908 / (1 + (PAR[:np.absolute(PAR-15).argmin()+1] / 261)**2.2 ) )
     
@@ -57,19 +58,19 @@ def NPQ_Corr (depth, PAR, MLD, Chla , bbp):
                                XB18[np.absolute(depth - MLD).argmin() +1  :np.absolute(depth - zPAR15).argmin() +1 ]    ), axis=None )
         
         # =============================================================================
-        # test if simoide method doesn't lead to smaller Fqc than Fq (that's impossible)
+        # test if sigmoide method doesn't lead to smaller Fqc than Fq (that's impossible)
         # If it's the case for more than 80% : Sackmann method is applied
         # =============================================================================
         if len(np.where(Fqc < Chla[:len(Fqc)])[0]) / len(Fqc) > 0.8 :
-            
-            Fqc, depth_Fqc = SO8(depth , MLD, Fratio , bbp) # application de SO8 au dessus de MLD
+            # Application above MLD
+            Fqc, depth_Fqc = SO8(depth , MLD, Fratio , bbp) 
             
         else:
-            
+            # Save depth array
             depth_Fqc = depth[:len(Fqc)]
             
     else :
-        
+        # Deep mixing case : SO8+ above zPAR15
         Fqc, depth_Fqc = SO8(depth , zPAR15 , Fratio , bbp) # Cas de deep mixing application de SO8+ donc au dessus de zPAR15
     
     
@@ -81,11 +82,11 @@ def NPQ_Corr (depth, PAR, MLD, Chla , bbp):
 
 def SO8 (depth , profondeur_corr , Fratio , bbp) :
                 
-        
-        Fratio_max = np.max(Fratio[:np.absolute(depth - profondeur_corr).argmin()+1])    # prend la plus haute valeur dans la couche d'eau au dessus de MLD
-        
+        # Find max value above MLD or zPAR=15
+        Fratio_max = np.max(Fratio[:np.absolute(depth - profondeur_corr).argmin()+1])    
+        # depth array
         depth_Fqc = depth[ : np.min(np.where( Fratio[:np.absolute(depth - profondeur_corr).argmin()+1] == Fratio_max))  +1 ]
-        
+        # Quenching correctiob
         Fqc = Fratio_max * bbp[:len(depth_Fqc)]
                
         return  Fqc, depth_Fqc   
@@ -93,17 +94,14 @@ def SO8 (depth , profondeur_corr , Fratio , bbp) :
 
 
 
-def X12 (depth , profondeur_corr , Chla_q) : # Prise du maximum de CHL dans la couche de mélange ML et extrapolation (ou au dessus de zPAR15 cas de x12+)
+def X12 (depth , profondeur_corr , Chla_q) : 
                 
-        
-
+        # Extraction of the maximum CHLa concentration within the mixing layer (ML) 
         Chla_max = np.max(Chla_q [:np.absolute(depth - profondeur_corr).argmin()+1])
-        
-       
+    
         depth_Fqc = depth[ : np.min(np.where( Chla_q[:np.absolute(depth - profondeur_corr).argmin()+1] == Chla_max) [0])  +1 ]
-        # print(depth_Fqc)
         
-        
+        # And extrapolation (or above zPAR15 in the case of X12+).
         Fqc = np.full(shape=len(depth_Fqc), fill_value=Chla_max)
                
         return  Fqc, depth_Fqc   
